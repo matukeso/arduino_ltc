f8b29b48f79d9ffa5fb26a86e31e24717cdf2020
matuken <mautken@jzkey.com>
2018-05-04 17:16:46 +0900

    for raspberry

--- ltc.c.ino
+++ ltc.c.ino
@@ -1,21 +1,19 @@
+//#include <SoftwareSerial.h>
+//SoftwareSerial toRasp(8,9);
+#define toRasp Serial1
 #include "ltc.h"
-#include <FlexiTimer2.h>
 
-const static byte outLTCpin = 4;
-const static byte intLTCclock = 7;
+
 const static byte inLTC = 3;
 const static byte TCEnableLED = 5;
 
 LTCFrame gFrame;
 //LTCFrame gInFrame;
 volatile byte inLTCbuf[10];
-
 unsigned char gOutBipNum;// ( 0 uptp 160 )
 bool gbOutBipBit;// High or Low
 volatile bool gbOutFrameCountUp;
-volatile bool gSending;
 
-void OnInterrupt();
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -25,44 +23,19 @@ void setup() {
 
   gOutBipNum = 0;
   gbOutBipBit = false;
-  gSending = false;
-
-
-  pinMode(outLTCpin, OUTPUT);
+  
 
   pinMode(TCEnableLED, OUTPUT);
   Serial.begin(115200);
-  Serial1.begin(9600);
+  toRasp.begin(9600);
 
-  //FlexiTimer2::set(1, 1 / 4800.0, OnInterrupt );
-  //FlexiTimer2::start();
 
-  //pinMode(intLTCclock, INPUT_PULLUP);
   pinMode(inLTC, INPUT);
   attachInterrupt(digitalPinToInterrupt(inLTC), OnInterruptReader, CHANGE );
-  //attachInterrupt(digitalPinToInterrupt(intLTCclock), OnInterrupt, RISING  );
-
-  gSending = true;
+  
+  digitalWrite(TCEnableLED, LOW);   // turn the LED on (HIGH is the voltage level)
 }
 
-// modify gbOutBipBit by sendig bit of gOutBipNum
-void SetCurrentOutputBit()
-{
-  //base clock(nBit = 0, 2, 4...158) always reverse output;
-  if ( (gOutBipNum  & 1) == 0 ) {
-    gbOutBipBit = !gbOutBipBit ;
-    return ;
-  }
-
-  //base clock + 1/2. change on/off by value
-  const char bytepos = gOutBipNum >> 4 ;
-  const char bitpos = (gOutBipNum >> 1 ) & 7;
-  const unsigned char curBitValue =  reinterpret_cast<unsigned char*>(&gFrame)[ bytepos ] >> bitpos;
-  // bit  on -> reverse output
-  if ( curBitValue & 1) {
-    gbOutBipBit = !gbOutBipBit ;
-  }
-}
 
 volatile bool in_frame_compl = 0;
 
@@ -133,7 +106,7 @@ void OnInterruptReader()
       if ( ltc_di == 10 ) {
         ltc_di = 0;
         if( ltc_frame[9] == 0b10111111 ){
-          memcpy( inLTCbuf, ltc_frame, 10 );
+          memcpy( (byte*)&inLTCbuf[0], ltc_frame, 10 );
           in_frame_compl = true;
         }
         else{
@@ -153,31 +126,10 @@ void OnInterruptReader()
     in_byte |= in_bit_pos;
   }
   else { 
-    Serial.print("S");
+ //   Serial.print("S");
   }
 
 }
-//4.8KHz Inteerupt( 80bit*30fps*2(biphase encoding) )
-void OnInterrupt()
-{
-  if ( !gSending ) {
-    //OH.We cant send.
-
-    return ;
-  }
-  SetCurrentOutputBit();
-  digitalWrite( outLTCpin, gbOutBipBit ? HIGH : LOW );
-  gOutBipNum++;
-
-  if ( gOutBipNum == 160 ) {
-    ltc_frame_increment(&gFrame, 30, 0);
-    gbOutFrameCountUp = true;
-    gOutBipNum = 0;
-  }
-
-}
-
-
 
 int cc = 0;
 void loop() {
@@ -193,38 +145,18 @@ void loop() {
     Serial.println("");
     prev_recv = now;
     bInSync = false;
-     digitalWrite(TCEnableLED, LOW);   // turn the LED on (HIGH is the voltage level)
     cc = 0;
   }
+  if(in_frame_compl){
+    byte ltc[10];
+    memcpy( ltc, (byte*)&inLTCbuf[0],10);
+    in_frame_compl = false;
+    toRasp.write(ltc, 10);
+  }
+
   if ( bInSync ) {
       digitalWrite(TCEnableLED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  }else{
+     digitalWrite(TCEnableLED, LOW);   // turn the LED on (HIGH is the voltage level)
   }
-
-  if (Serial1.available() > 0) {
-    // read the incoming byte:
-    int incomingByte = Serial1.read();
-    LTCFrame f;
-    memcpy( &f,  inLTCbuf, 10 );
-    if ( incomingByte == 2 ) {
-      char buf[32];
-      int di = Serial1.readBytesUntil(';', buf, 32);
-      buf[di] = 0;
-      char ltvf[16];
-      ltc_frame_format( ltvf, &f);
-
-      Serial.write( ltvf );
-//      Serial.print("(");
-//      Serial.print(intNum, DEC);
-//      Serial.print(")");
-
-      Serial.print( " = " );
-      Serial.write(buf, di);
-      Serial.println("");
-    }
-
-  }
-
-  //halt();
-  // put your main code here, to run repeatedly:
-  //OnInterrupt();
 }
